3723df7efa75cb0b64062b8540c5c117b78260d4
Rizqika Mulia Pratama <rizqikapratamaa@gmail.com>
2024-01-06 23:39:29 +0700

    fix: fix typo

--- mediq_backend/app.js
+++ mediq_backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 6000;
 
-app.unsubscribe(express.json());
+app.use(express.json());
 
 app.listen(port, () => {
     console.log('server is running at port ', port);
